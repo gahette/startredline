bafc7d250d21445fb0209cbbcf3de21c13b0c796
jean-baptisteminier <gahette@icloud.com>
2021-12-11 16:31:16 +0100

    live2

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// Commentaire sur une ligne
+/* Commentaire
+sur plusieurs
+ lignes
+ */
+
+// Créer un livre
+
+let book;
+let title = "Les Misérables"
+let author = "Victor hugo";
+let price = 14.95;
+let category = "drame";
+let releaseDate = new date("1997, 05, 23");
+let nbSales = 345;
+
