34231e905411ceaf4a1bb2077691242712d9cd3c
jean-baptisteminier <gahette@icloud.com>
2021-12-12 16:02:16 +0100

    live3

--- javascript.js
+++ javascript.js
@@ -4,13 +4,23 @@ sur plusieurs
  lignes
  */
 
-// Créer un livre
+/* ******* Créer un livre avec des variables (cours 2) ******* */
 
 let book;
-let title = "Les Misérables"
+let title = "Les Misérables";
 let author = "Victor hugo";
 let price = 14.95;
 let category = "drame";
-let releaseDate = new date("1997, 05, 23");
+let releaseDate = new Date("1997, 05, 23");
 let nbSales = 345;
 
+/* ******* Créer un livre avec des objets et tableaux (cours 3) ****** */
+let bookst = []; // importer le JSON
+
+// todo import JSON
+
+// Tableau d’objet pour le panier
+let cart = [
+  {id: "0", qqt: 1},
+  {id: "1", qqt: 2},
+];
